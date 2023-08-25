db0e43484812cb871f6c1484e75408c33fbbf825
MakerTechno <102357646+MakerTechno@users.noreply.github.com>
2023-08-25 21:24:57 +0800

    Now you can try to run it.

--- src/ngit/maker/recorder/Launcher.java
+++ src/ngit/maker/recorder/Launcher.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 
 public class Launcher {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-
-        });
+        SwingUtilities.invokeLater(TaskManager::new);
     }
 }
